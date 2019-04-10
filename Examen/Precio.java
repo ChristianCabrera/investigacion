da49f0d1cb861ba9915fa201c193a809dbb5a1a1
ChristianCabrera <48428246+ChristianCabrera@users.noreply.github.com>
2019-04-10 10:11:51 -0500

    examen primer parcial

---
+++ Examen/Precio.java
@@ -0,0 +1,12 @@
+package Examen;
+import java.util.Scanner;
+/**
+ * 
+ * @author Christian
+ */
+public class Precio extends AreaRectangulo{
+    public static double Calcular (double Precio){
+     double Area = CalcularArea();
+     return Area * Precio;
+    }        
+    }
